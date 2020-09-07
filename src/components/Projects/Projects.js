8eba98f0a0892da143226c6de0110d1abe30a381
479BetaCoder <ravikumar.pilla479@gmail.com>
2020-09-06 21:07:57 -0400

    project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Projects.css";
 
 export default class Projects extends React.Component {
   render() {
     return (
       <div>
-        <h1>My Projects!</h1>
+        <section className="projectsHdrContainer">
+        <h1> Projects: Overview of some of my academic and personal projects
+        </h1>
+        </section>
+        <section className="projectBorder projectList">
+          <div className="projectsContainer">
+          <h3>Recent Projects:</h3>  
+          <section className="projectCard">
+          </section>
+          <section className="projectDesc">
+          </section>
+          <section className="projectTech">
+          </section>
+          </div>
+        </section>
       </div>
     );
   }
